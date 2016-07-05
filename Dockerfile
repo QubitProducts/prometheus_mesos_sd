a3061f522f2828e2eca9bdd5a798137f6c824879
Tristan Colgate <tristan@qubit.com>
2016-07-05 09:27:20 +0100

    Add CLI args for consoles and temaplates

--- Dockerfile
+++ Dockerfile
@@ -12,6 +12,7 @@ ADD docker_run.sh /usr/local/bin/docker_run.sh
 RUN mkdir -p /opt/prometheus/conf/mesos-groups
 RUN mkdir -p /opt/prometheus/conf/rules
 RUN mkdir -p /opt/prometheus/conf/alerts
+RUN mkdir -p /opt/prometheus/conf/consoles/libs
 ADD prometheus.yaml.tmpl /opt/prometheus/conf/prometheus.yaml.tmpl
 #ADD alertmanager.yaml /opt/prometheus/conf/alertmanager.yaml
 
--- docker_run.sh
+++ docker_run.sh
@@ -7,4 +7,4 @@ MARATHON_URL=$1
 
 cat $PROMCONF/prometheus.yaml.tmpl | sed -e "s#MARATHON_URL#${MARATHON_URL}#g" > $PROMCONF/prometheus.yaml
 /go/bin/prometheus_mesos_sd -logtostderr -mesos-exporter /go/bin/mesos-exporter -file.master $PROMCONF/mesos-groups/master.json -file.slaves $PROMCONF/mesos-groups/slaves.json $2 &
-/go/bin/prometheus -config.file $PROMCONF/prometheus.yaml $PROMFLAGS
+/go/bin/prometheus -config.file $PROMCONF/prometheus.yaml -web.console.templates /opt/prometheus/conf/consoles -web.console.libraries /opt/prometheus/conf/cnosoles/lib $PROMFLAGS
