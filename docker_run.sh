8c302428e830eb986f8a7ead29a79f7ab1bad31a
Tristan Colgate <tristan@qubit.com>
2016-07-13 16:52:01 +0100

    Let us use mesos specific persistent storage

--- docker_run.sh
+++ docker_run.sh
@@ -5,6 +5,10 @@ env
 PROMCONF=/opt/prometheus/conf
 MARATHON_URL=$1
 
+if [ ! -z "$MESOS_STORAGE" && ! -z "$MESOS_SANDBOX"]; then
+  PROMFLAGS="$PROMFLAGS -storage.local.path	$MESOS_SANDBOX/$MESOS_STORAGE"
+fi
+
 cat $PROMCONF/prometheus.yaml.tmpl | sed -e "s#MARATHON_URL#${MARATHON_URL}#g" > $PROMCONF/prometheus.yaml
 /go/bin/prometheus_mesos_sd -logtostderr -file.master $PROMCONF/mesos-groups/master.json -file.slaves $PROMCONF/mesos-groups/slaves.json $2 &
 /go/bin/prometheus -config.file $PROMCONF/prometheus.yaml -web.console.templates /opt/prometheus/conf/consoles -web.console.libraries /opt/prometheus/conf/cnosoles/lib $PROMFLAGS
